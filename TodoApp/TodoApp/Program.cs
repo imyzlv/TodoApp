3b23b72826083456fe1f11a1b4de2023e0c93cec
Imants Treidis <imants.treidis@gmail.com>
2022-05-18 21:53:10 +0300

    Development step comments

--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -20,3 +20,14 @@ app.MapRazorPages();
 
 app.Run();
 
+//MVP, Jāuztaisa iespēja ievadīt jaunus uzdevumus
+//1. Uztaisīt objektus/klases lai pievienotu uzdevumus
+//2. Uztaisīt klasi/objektus lietotāja izveidei
+//3. Datubāze
+// -> lietājiem >> Username, password
+// -> Numurs, Uzdevums, Datums, Lietotājs, Nosaukums
+
+
+//Bāze
+//2 tabulas -> Imants parole
+//Numurs, Uzdevums, Datums, Lietotājs, Nosaukums -> Imants
