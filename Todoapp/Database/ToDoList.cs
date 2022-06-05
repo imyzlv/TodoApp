ba62e075ae2dea8662ff44cbe1571216ab9b841b
EdgarsB <edgars.beerzinjsh@gmail.com>
2022-06-05 15:39:36 +0300

    Seperated ToDoListController from HomeController; Updated ToDoList.cs with parameters; Tried to implement startup data in ToDoList database in DbContext.cs; Seperated ToDoList view

--- Todoapp/Database/ToDoList.cs
+++ Todoapp/Database/ToDoList.cs
@@ -1,6 +1,22 @@
-﻿namespace Todoapp.Database
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Todoapp.Database
 {
     public class ToDoList
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string TaskText { get; set; }
+
+        [Required]
+        public string TaskList { get; set; }
+
+        public int TaskLevel { get; set; }
+
+        public static DateTime Now { get; }
     }
 }
