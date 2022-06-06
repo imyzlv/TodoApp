987e75063dc66a36212eb80796d86575b33867a0
Imants Treidis <imants.treidis@gmail.com>
2022-06-06 23:31:15 +0300

    Add unit test
    Add a simple unit test to check whether the ApplicationName is set correctly in appsettings.json

---
+++ Todoapp.Tests/UnitTest_1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Todoapp.Tests;
+
+public class TodoappTests
+{
+    [Fact]
+    public void GetAppName_DuringAppStartup_GetsNameFromConfig()
+    {
+        // Arrange
+        string appName = "Todo application";
+        // Act
+        // Assert
+
+        //var conf = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appsettings.json", false, true).Build();
+
+        var config = new Todoapp.Configuration.TodoConfiguration(new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appsettings.json", false, true).Build());
+        Assert.Equal(appName, config.GetAppName());
+    }
+}
--- Todoapp/Configuration/TodoConfiguration.cs
+++ Todoapp/Configuration/TodoConfiguration.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 namespace Todoapp.Configuration
 {
-	public class TodoConfiguration
-	{
-		private readonly IConfiguration _config;
+    public class TodoConfiguration
+    {
+        private readonly IConfiguration _config;
 
-		public TodoConfiguration(IConfiguration config)
-		{
-			_config = config;
-		}
-
-		public string GetAppName()
+        public TodoConfiguration(IConfiguration config)
         {
-			return _config["ApplicationName"];
+            _config = config;
         }
-	}
+
+        public string GetAppName()
+        {
+            return _config["ApplicationName"];
+        }
+    }
 }
 
